7d224b7fe071b5305763bc4d24c82cbeeb313ba1
srgi0 <dss181@gmail.com>
2024-08-05 11:42:06 +0300

    add views

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ docker compose -f docker-compose.nginx.yml up -d
 ```
 Проверить dry-run
 ```bash
-docker compose -f docker-compose.nginx.yml exec certbot /bin/sh -c 'certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d "$DOMAIN"'
+docker compose -f docker-compose.nginx.yml exec certbot /bin/sh -c 'certbot certonly --verbose --webroot --webroot-path /var/www/certbot/ --dry-run -d "$DOMAIN"'
 ```
 Получить сертификат
 ```bash
--- neuro_crm/urls.py
+++ neuro_crm/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("neuro_crm.api.urls"), name="api"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
