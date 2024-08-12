7ba7e185140f63c17ea7bfe896f866cf3d71fd6f
srgi0 <dss181@gmail.com>
2024-08-12 11:25:52 +0300

    remove deprecated distutils

--- neuro_crm/core/tests.py
+++ neuro_crm/core/tests.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from neuro_crm.core.tasks import debug_task
+from neuro_crm.core.utils import strtobool
 
 
 @patch("neuro_crm.core.celery.Task.apply_async")
@@ -9,3 +10,8 @@ def test_debug_task(apply_mock):
     assert apply_mock.call_count == 1
 
     assert debug_task.run() is True
+
+
+def test_strtobool():
+    assert strtobool("False") is False
+    assert strtobool("True") is True
--- neuro_crm/settings/base.py
+++ neuro_crm/settings/base.py
@@ -14,12 +14,11 @@ import os
 import sys
 from pathlib import Path
 
-from distutils.util import strtobool
+from neuro_crm.core.utils import strtobool
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
