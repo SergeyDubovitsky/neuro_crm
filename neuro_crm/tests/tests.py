4bf5c74efd1756115e269e911e9389234303aed4
srgi0 <dss181@gmail.com>
2024-08-12 18:15:25 +0300

    add bootstrap

--- neuro_crm/tests/tests.py
+++ neuro_crm/tests/tests.py
@@ -7,3 +7,9 @@ async def test_healthcheck(async_client):
     resp = await async_client.get(reverse("api:healthcheck"))
     assert resp.status_code == 200
     assert resp.json()
+
+
+@pytest.mark.asyncio
+async def test_home(async_client):
+    resp = await async_client.get(reverse("home"))
+    assert resp.status_code == 200
